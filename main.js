70ae62da50f8ed965dcd969eac73f2104f82c104
Jesse Chong <jessechong@pursuit.org>
2023-12-17 21:49:16 -0500

    continuing tutorial

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
 import './style.css'
 import Phaser from 'phaser';
+
+const sizes = {
+  width: 500,
+  height: 500
+}
+
+class GameScene extends Phaser.Scene{
+  constructor(){
+    super("scene-game")
+  }
+
+  preload(){
+    this.load.image("bg", "/assets/bg.png")
+  }
+  create(){
+    this.add.image(0, 0, "bg").setOrigin(0, 0)
+  }
+  update(){}
+}
+
+const config = {
+  type: Phaser.WEBGL,
+  width: sizes.width,
+  height: sizes.height,
+  canvas: gameCanvas
+}
+
+const game = new Phaser.Game(config);
