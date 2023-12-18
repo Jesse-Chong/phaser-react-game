991b12b66276c170e9e7c2ce214519f47a0acda1
Jesse Chong <jessechong@pursuit.org>
2023-12-17 21:59:28 -0500

    Let user us arrowk keys to move basket left and right

--- main.js
+++ main.js
@@ -6,30 +6,38 @@ const sizes = {
   height: 500
 }
 
-const speedDown = 300;
+const speedDown = 300
 
 class GameScene extends Phaser.Scene{
   constructor(){
-    super("scene-game");
-    this.player;
-    this.cursor;
-    this.playerSpeed=speedDown+50;
+    super("scene-game")
+    this.player
+    this.cursor
+    this.playerSpeed=speedDown+50
   }
 
   preload(){
-    this.load.image("bg", "/assets/bg.png");
-    this.load.image("basket", "/assets/basket.png");
+    this.load.image("bg", "/assets/bg.png")
+    this.load.image("basket", "/assets/basket.png")
   }
   create(){
     this.add.image(0, 0, "bg").setOrigin(0, 0)
     this.player = this.physics.add.image(0, sizes.height - 100, "basket").setOrigin(0, 0)
     // prevent basket from being affected by gravity (physics) ^^
-    this.player.setImmovable(true);
-    this.player.body.allowGravity = false;
+    this.player.setImmovable(true)
+    this.player.body.allowGravity = false
 
-    this.cursor=this.input.keyboard.createCursorKeys();
+    this.cursor=this.input.keyboard.createCursorKeys()
   }
   update(){
+        const {left, right} = this.cursor;
+    if (left.isDown) {
+      this.player.setVelocityX(-this.playerSpeed);
+    } else if (right.isDown) {
+      this.player.setVelocityX(this.playerSpeed);
+    } else {
+      this.player.setVelocityX(0);
+    }
   }
 }
 
